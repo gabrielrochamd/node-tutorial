786d45bd443b17a4f931cff4b7bbfde79b3f52aa
Gabriel Rocha <gabrielmrocha@outlook.com.br>
2022-04-15 01:12:02 -0300

    Lesson 2

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -1,8 +1,18 @@
+const bcrypt = require('bcryptjs');
 const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const authConfig = require('../config/auth.json');
 
 const User = require('../model/User');
 
 const router = express.Router();
+
+function generateToken(params = {}) {
+  return jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400
+  });
+}
 
 router.post('/register', async (req, res) => {
   const { email } = req.body;
@@ -15,10 +25,36 @@ router.post('/register', async (req, res) => {
 
     user.password = undefined;
 
-    return res.send(user);
+    return res.send({
+      user,
+      token: generateToken({ id: user.id })
+    });
   } catch (e) {
     return res.status(400).send({ message: 'Registration failed.' });
   }
+});
+
+router.post('/authenticate', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user)
+    return res.status(400).send({ message: 'User not found' });
+  
+  if (!await bcrypt.compare(password, user.password))
+    return res.status(400).send({ message: 'Invalid password' });
+  
+  user.password = undefined;
+
+  const token = jwt.sign({ id: user.id }, authConfig.secret, {
+    expiresIn: 86400
+  });
+  
+  return res.send({
+    user,
+    token: generateToken({ id: user.id })
+  });
 });
 
 module.exports = app => app.use('/auth', router);
--- src/index.js
+++ src/index.js
@@ -7,5 +7,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 require('./controller/auth')(app);
+require('./controller/project')(app);
 
 app.listen(3000);
